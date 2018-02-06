d552aeb0cde3032f93663efda5cf0c2472afb6cd
tmed99 <tmedina-mercado@ecc-jaguar.csuchico.edu>
2018-02-05 17:46:30 -0800

    Tells what whole number is biggest

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,13 +5,22 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
+  int number;
+  int number2; 
   cout<<"Please enter a whole number:\n";
-  
+  cin>>number;  
   cout<<"Please enter another whole number:\n";
-
-  cout<<"Of those two numbers, the biggest is: ";
+  cin>>number2;
+  if (number >= number2)
+  {
+  cout<<"Of those two numbers, the biggest is:"<<number<<".";
+  }
+  else
+  {
+  cout<<"Of these two numbers, the biggest is:"<<number2<<".";
+  }
   cout<<endl<<"Thank you for playing.\n";
 
+  
   return 0;
 }
