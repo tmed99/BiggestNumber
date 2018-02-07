04342dcfa65b0cec17c5cb0b8d97cfb2f5974046
tmed99 <tmedina-mercado@ecc-jaguar.csuchico.edu>
2018-02-06 22:47:52 -0800

    Keeps track of biggest number

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -6,19 +6,21 @@ using namespace std; //so we don't need the prefix std::
 int main()
 {
   int number;
-  int number2; 
+  int number2;
+  int biggest;
   cout<<"Please enter a whole number:\n";
   cin>>number;  
   cout<<"Please enter another whole number:\n";
   cin>>number2;
   if (number >= number2)
   {
-  cout<<"Of those two numbers, the biggest is:"<<number<<".";
+  (biggest = number);
   }
   else
   {
-  cout<<"Of these two numbers, the biggest is:"<<number2<<".";
+  (biggest = number2);
   }
+  cout<<"Of those two, the biggest number is "<<biggest<<".";
   cout<<endl<<"Thank you for playing.\n";
 
   
